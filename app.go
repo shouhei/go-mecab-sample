cf89648ad502b1e895c74ce74e17179569b7faaf
山口将平 <shouhei.yamaguchi.be@gmail.com>
2016-05-25 06:08:13 +0900

    implemented? cos similarity

--- app.go
+++ app.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"net/http"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
+	set "github.com/deckarep/golang-set"
 	"github.com/djimenez/iconv-go"
 	"github.com/shogo82148/go-mecab"
 )
@@ -67,5 +69,59 @@ func main() {
 		}
 		urlWordMap[url] = words
 	}
-	fmt.Println(urlWordMap)
+	uniqueWords := set.NewSet()
+	for _, words := range urlWordMap {
+		for _, word := range words {
+			uniqueWords.Add(word)
+		}
+	}
+	flags := [][]int{}
+	for _, words := range urlWordMap {
+		flags = append(flags, MakeFlag(ToStringSlice(uniqueWords.ToSlice()), words))
+	}
+	innerProd := 0.0
+	for i, _ := range flags[0] {
+		innerProd = innerProd + float64((flags[0][i] * flags[1][i]))
+	}
+	result := innerProd / (Norm(flags[0]) * Norm(flags[1]))
+	fmt.Println(result)
+
+}
+
+func ToStringSlice(a []interface{}) []string {
+	var retData []string
+	for _, val := range a {
+		retData = append(retData, val.(string))
+	}
+	return retData
+}
+
+func Norm(a []int) float64 {
+	sum := 0.0
+	for _, val := range a {
+		sum = sum + math.Abs(float64(val*val))
+	}
+	return sum
+}
+
+func MakeFlag(unique []string, words []string) []int {
+	var flags []int
+	for _, uniqueWord := range unique {
+		if Contains(words, uniqueWord) {
+			flags = append(flags, 1)
+		} else {
+			flags = append(flags, 0)
+		}
+	}
+	return flags
+}
+
+func Contains(slice []string, item string) bool {
+	set := make(map[string]struct{}, len(slice))
+	for _, s := range slice {
+		set[s] = struct{}{}
+	}
+
+	_, ok := set[item]
+	return ok
 }
